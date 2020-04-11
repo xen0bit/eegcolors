db2aa2b9c6af621153f7837226e4d922c0e53712
xen0bit <xenobitbugs@outlook.com>
2020-04-11 14:13:11 -0400

    test

--- DNN_EEG_Color_classifier.py
+++ DNN_EEG_Color_classifier.py
@@ -69,36 +69,51 @@ model = tf.keras.Sequential([
   tf.keras.layers.Dense(3)
 ])
 
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-def loss(model, x, y):
-    #print(model, x, y)
-    y_ = model(x)
-    return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+def loss(model, x, y, training):
+  # training=training is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  y_ = model(x, training=training)
 
+  return loss_object(y_true=y, y_pred=y_)
+
+
+l = loss(model, features, labels, training=False)
+print("Loss test: {}".format(l))
+
+
+# def loss(model, x, y):
+#     #print(model, x, y)
+#     y_ = model(x)
+#     return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+
+
+# def grad(model, inputs, targets):
+#     with tf.GradientTape() as tape:
+#         loss_value = loss(model, inputs, targets)
+#     return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
 def grad(model, inputs, targets):
-    with tf.GradientTape() as tape:
-        loss_value = loss(model, inputs, targets)
-    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets, training=True)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 
-global_step = tf.train.get_or_create_global_step()
-
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
 loss_value, grads = grad(model, features, labels)
 
-print("Step: {}, Initial Loss: {}".format(global_step.numpy(),
+print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
                                           loss_value.numpy()))
 
-optimizer.apply_gradients(zip(grads, model.variables), global_step)
+optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-print("Step: {},         Loss: {}".format(global_step.numpy(),
-                                          loss(model, features, labels).numpy()))
-
+print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
+                                          loss(model, features, labels, training=True).numpy()))
 ## Note: Rerunning this cell uses the same model variables
 
-# keep results for plotting
+# Keep results for plotting
 train_loss_results = []
 train_accuracy_results = []
 
@@ -108,25 +123,25 @@ for epoch in range(num_epochs):
     epoch_loss_avg = tf.keras.metrics.Mean()
     epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
 
-
-    # Training loop - using batches of 32
+  # Training loop - using batches of 32
     for x, y in train_dataset:
         # Optimize the model
         loss_value, grads = grad(model, x, y)
-        optimizer.apply_gradients(zip(grads, model.variables),
-                                  global_step)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
         # Track progress
-        epoch_loss_avg(loss_value)  # add current batch loss
-        # compare predicted label to actual label
-        epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+        epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+        # Compare predicted label to actual label
+        # training=True is needed only if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        epoch_accuracy.update_state(y, model(x, training=True))
 
-    # end epoch
-    train_loss_results.append(epoch_loss_avg.result())
-    train_accuracy_results.append(epoch_accuracy.result())
+        # End epoch
+        train_loss_results.append(epoch_loss_avg.result())
+        train_accuracy_results.append(epoch_accuracy.result())
 
-    if epoch % 50 == 0:
-        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+        if epoch % 50 == 0:
+            print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
                                                                     epoch_loss_avg.result(),
                                                                     epoch_accuracy.result()))
 
@@ -145,23 +160,26 @@ test_url = "https://raw.githubusercontent.com/xen0bit/eegcolors/master/merged_la
 test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
                                   origin=test_url)
 
-test_dataset = tf.contrib.data.make_csv_dataset(
+test_dataset = tf.data.experimental.make_csv_dataset(
     test_fp,
     batch_size,
     column_names=column_names,
-    label_name='Color',
+    label_name='species',
     num_epochs=1,
     shuffle=False)
 
 test_dataset = test_dataset.map(pack_features_vector)
 
-test_accuracy = tfe.metrics.Accuracy()
+test_accuracy = tf.keras.metrics.Accuracy()
 
 for (x, y) in test_dataset:
-  logits = model(x)
-  prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
-  test_accuracy(prediction, y)
+    # training=False is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    logits = model(x, training=False)
+    prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
+    test_accuracy(prediction, y)
 
 print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+
 
 
